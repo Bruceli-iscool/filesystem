3396dec3d0c5f68aaf9dee85dcdc6c94837f227e
Bruce Li <isanoob1897@outlook.com>
2025-09-10 15:08:05 +0000

    add argument processing

--- src/filesystem.java
+++ src/filesystem.java
@@ -1,7 +1,16 @@
 import java.util.*;
+import java.io.File;
 
 public class filesystem {
     public static void main(String args[]) {
-
+	if (args.lenght ==0) {
+	    System.out.println("Filesystem Usage:\n filesystem [filepath] [args])");
+	} else {
+	    process(args[0], args[1]);
+	}
+    }
+    public static void process(String path, String args) {
+    	// todo: handle fileystem processing
+	
     }
 }
